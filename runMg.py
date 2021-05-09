cbb8ab3c20ced637f520130dd3ef931e176f5b80
Pythonshik <83823440+BiyaslanMokaa@users.noreply.github.com>
2021-05-09 12:52:49 +0300

    Update runMg.py

--- runMg.py
+++ runMg.py
@@ -1,27 +1,28 @@
 import pandas as pd
-# from mainParserMagnolia import parser
+ from mainParserMagnolia import parser
 
-# def RUN():
-# 	global parser
+ def RUN():
+ 	global parser
 	
-# 	del parser['Name'][len(parser['Name']) - 1]
+	del parser['Name'][len(parser['Name']) - 1]
 
-# 	parser = pd.DataFrame(parser)
+	parser = pd.DataFrame(parser)
 
-# 	for step in range(len(parser)):
-# 		if list(parser['Name']).count(parser['Name'][step]) > 1:
-# 			parser.drop(step, inplace=True)
+ 	for step in range(len(parser)):
+ 		if list(parser['Name']).count(parser['Name'][step]) > 1:
+ 			parser.drop(step, inplace=True)
 		
 
-# 	parser.to_csv("Magnolia.csv", index=None)
+ 	parser.to_csv("Magnolia.csv", index=None)
 
 
-# if __name__ == "__main__":
-# 	RUN()
-df = pd.read_csv("Magnolia.csv")
-# print(list(df['Measure']))
+if __name__ == "__main__":
+	RUN()
+	
+	df = pd.read_csv("Magnolia.csv")
+	# print(list(df['Measure']))
 
-df = df.replace('<span class="price_measure">/шт</span>', "/шт")
-df = df.replace('<span class="price_measure">/кг</span>', "/кг")
+	df = df.replace('<span class="price_measure">/шт</span>', "/шт")
+	df = df.replace('<span class="price_measure">/кг</span>', "/кг")
 
-df.to_csv("Magnolia.csv", index=None)
+	df.to_csv("Magnolia.csv", index=None)
